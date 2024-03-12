79e5957652231b3514c4763e02d1a6d266e78385
Sunwoo Song <sunwsong@c1r5s7.42seoul.kr>
2024-03-12 20:05:39 +0900

    initial commit && ex00 fin

---
+++ ex00/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "whatever.hpp"
+
+// #include <vector>
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    // std::vector<int> v1, v2;
+    // v1.push_back(1);
+    // v1.push_back(2);
+    // v2.push_back(1);
+    // v2.push_back(3);
+
+    // ::swap(v1, v2);
+    // std::cout << ::min( v1, v2 ).at(1) << std::endl;
+    // std::cout << ::max( v1, v2 ).at(1) << std::endl;
+
+    float f = 42.42;
+    double g = 21.21;
+
+    // ::swap(f, g);
+
+    return 0;
+}
