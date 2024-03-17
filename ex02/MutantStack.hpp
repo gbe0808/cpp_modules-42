a152da78e10ca3af27166ce2eb08d235389bdc2d
Sunwoo Song <sunwsong@c3r2s6.42seoul.kr>
2024-03-17 15:07:15 +0900

    ex02 works

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
-class MutantStack {
+#include <stack>
+#include <deque>
 
-}
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack &ref) : std::stack<T>(ref) {}
+    MutantStack &operator=(const MutantStack &ref) {
+        std::stack<T>::operator=(ref);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    typedef typename std::deque<T>::iterator iterator;
+
+    iterator begin() {
+        return this->c.begin();
+    }
+
+    iterator end() {
+        return this->c.end();
+    }
+};
