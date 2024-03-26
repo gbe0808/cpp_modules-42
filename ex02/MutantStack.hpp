29bd53c0ba630519432db2cf88f4983af630d74f
Sunwoo Song <sunwsong@c3r3s6.42seoul.kr>
2024-03-26 20:23:34 +0900

    ex02 tpp

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,21 +6,15 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
 public:
-    MutantStack() : std::stack<T>() {}
-    MutantStack(const MutantStack &ref) : std::stack<T>(ref) {}
-    MutantStack &operator=(const MutantStack &ref) {
-        std::stack<T>::operator=(ref);
-        return *this;
-    }
-    ~MutantStack() {}
+    MutantStack();
+    MutantStack(const MutantStack &ref);
+    MutantStack &operator=(const MutantStack &ref);
+    ~MutantStack();
 
     typedef typename std::deque<T>::iterator iterator;
 
-    iterator begin() {
-        return this->c.begin();
-    }
-
-    iterator end() {
-        return this->c.end();
-    }
+    iterator begin();
+    iterator end();
 };
+
+#include "MutantStack.tpp"
---
+++ ex02/MutantStack.tpp
@@ -0,0 +1,24 @@
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &ref) : std::stack<T>(ref) {}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &ref) {
+    std::stack<T>::operator=(ref);
+    return *this;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack() {}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() {
+    return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() {
+    return this->c.end();
+}
